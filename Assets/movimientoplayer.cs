841754cb4456071f3dad1a316658e39de9bec0ab
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-06-01 13:03:32 -0300

    movimiento personaje

---
+++ Assets/movimientoplayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoplayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speedZ;
+    public float speedgiro;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speedZ);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speedZ);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, speedgiro, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -speedgiro, 0);
+        }
+    }
+}
